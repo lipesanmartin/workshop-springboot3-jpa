22f364c6a1f886bdf7506b008fd1318701dc06c5
Felipe Sanmartin <lipesanmartin@gmail.com>
2023-09-28 15:25:41 -0300

    Exception handling - update

--- src/main/java/com/sanmartindev/course/services/UserService.java
+++ src/main/java/com/sanmartindev/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.sanmartindev.course.repositories.UserRepository;
 import com.sanmartindev.course.services.exceptions.DatabaseException;
 import com.sanmartindev.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
